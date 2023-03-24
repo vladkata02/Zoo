0e495f9c9d08330cf1c1ec6325241dd26f1a77e7
Владислав Бошнаков <vboshnakov@david.bg>
2023-03-24 23:12:04 +0200

    Finish home page

--- my-zoo-app/src/components/Home/Home.js
+++ my-zoo-app/src/components/Home/Home.js
@@ -1,27 +1,62 @@
 import React from "react";
+import { Carousel, Container } from "react-bootstrap";
 
 export const Home = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <img src="https://efgcr95ic65.exactdn.com/wp-content/uploads/2020/09/tiger-in-zoo-trail-scaled.jpg?strip=all&lossy=1&ssl=1"
-            style={{ height: "700px", width: "1287px" }} className="img-fluid" alt="Zoo" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://www.oregonzoo.org/sites/default/files/2020/05/01/H_ring-tailed-lemur.jpg" className="img-thumbnail" alt="Thumbnail 1" />
-          </div>
-          <div className="col-md-4">
-            <img src="https://nationalzoo.si.edu/sites/default/files/animals/giantpanda-010.jpg" className="img-thumbnail" alt="Thumbnail 2" />
-          </div>
-          <div className="col-md-4">
-            <img src="https://cms.londonzoo.org/sites/default/files/styles/responsive/public/1440/729/1/2022-11/Asim-at-London-Zoo.jpg?itok=sxChnnU-" className="img-thumbnail homeSmallPictures" alt="Thumbnail 3" />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container fluid>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://efgcr95ic65.exactdn.com/wp-content/uploads/2020/09/tiger-in-zoo-trail-scaled.jpg?strip=all&lossy=1&ssl=1"
+            alt="First slide"
+            style={{ height: "700px", objectFit: "cover" }}
+          />
+          <Carousel.Caption className="p-5 text-white" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+            <h3>Welcome to the Zoo</h3>
+            <p>Explore the amazing world of animals</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.oregonzoo.org/sites/default/files/2020/05/01/H_ring-tailed-lemur.jpg"
+            alt="Second slide"
+            style={{ height: "700px", objectFit: "cover" }}
+          />
+
+          <Carousel.Caption className="p-5 text-white" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+            <h3>Ring-tailed lemurs</h3>
+            <p className="lead">Get up close and personal with these curious creatures</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://nationalzoo.si.edu/sites/default/files/animals/giantpanda-010.jpg"
+            alt="Third slide"
+            style={{ height: "700px", objectFit: "cover" }}
+          />
+
+          <Carousel.Caption className="p-5 text-white" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+            <h3>Giant pandas</h3>
+            <p className="lead">See these rare and beloved animals in person</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://cms.londonzoo.org/sites/default/files/styles/responsive/public/1440/729/1/2022-11/Asim-at-London-Zoo.jpg?itok=sxChnnU-"
+            alt="Fourth slide"
+            style={{ height: "700px", objectFit: "cover" }}
+          />
+
+          <Carousel.Caption className="p-5 text-white" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+            <h3>Tigers</h3>
+            <p className="lead">Come and meet our majestic tigers, with their stunning stripes and powerful presence.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
   );
 };
