e9cf916174fdcf34e1bcf32de0bde27731d6b036
Владислав Бошнаков <vboshnakov@david.bg>
2023-03-26 16:05:55 +0300

    Fix header links location

--- my-zoo-app/src/components/Header/Header.js
+++ my-zoo-app/src/components/Header/Header.js
@@ -1,12 +1,18 @@
-import React from "react";
+import { useContext, React } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export const Header = () => {
+  const { isAuthenticated, userEmail } = useContext(AuthContext);
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light custom-nav">
       <div className="container">
-        <Link className="nav-link rounded custom-logo" to="/" style={{ transition: "background-color 0.2s ease-in-out" }}>
+        <Link
+          className="nav-link rounded custom-logo"
+          to="/"
+          style={{ transition: "background-color 0.2s ease-in-out" }}
+        >
           Zoo
         </Link>
         <button
@@ -23,34 +29,62 @@ export const Header = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link className="nav-link rounded" to="/" style={{ transition: "background-color 0.2s ease-in-out" }}>
+              <Link
+                className="nav-link rounded"
+                to="/"
+                style={{ transition: "background-color 0.2s ease-in-out" }}
+              >
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link rounded" to="/posts" style={{ transition: "background-color 0.2s ease-in-out" }}>
+              <Link
+                className="nav-link rounded"
+                to="/posts"
+                style={{ transition: "background-color 0.2s ease-in-out" }}
+              >
                 Posts
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link rounded" to="/tickets" style={{ transition: "background-color 0.2s ease-in-out" }}>
-                Buy Tickets
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link rounded" to="/admin" style={{ transition: "background-color 0.2s ease-in-out" }}>
-                Admin
-              </Link>
-            </li>
+            {isAuthenticated && (
+              <>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link rounded"
+                    to="/tickets"
+                    style={{ transition: "background-color 0.2s ease-in-out" }}
+                  >
+                    Buy Tickets
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link rounded"
+                    to="/admin"
+                    style={{ transition: "background-color 0.2s ease-in-out" }}
+                  >
+                    Admin
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
           <ul className="navbar-nav ml-auto login">
             <li className="nav-item">
-              <Link className="nav-link rounded" to="/login" style={{ transition: "background-color 0.2s ease-in-out" }}>
+              <Link
+                className="nav-link rounded login-link"
+                to="/login"
+                style={{ transition: "background-color 0.2s ease-in-out" }}
+              >
                 Login
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link rounded" to="/register" style={{ transition: "background-color 0.2s ease-in-out" }}>
+            <li className="nav-item register">
+              <Link
+                className="nav-link rounded"
+                to="/register"
+                style={{ transition: "background-color 0.2s ease-in-out" }}
+              >
                 Register
               </Link>
             </li>
